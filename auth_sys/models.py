d8aaf93854873a5d8c5f6d156cfb00a26999f788
= <yarovenkovova2008@gmail.com>
2024-11-18 08:05:19 -0330

    CustomUser added

--- auth_sys/models.py
+++ auth_sys/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+class CustomUser(AbstractBaseUser):
+    username = None
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
