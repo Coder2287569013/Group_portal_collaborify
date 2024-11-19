80023ebe006781bdb4c985422b1c7c1c5e762119
= <yarovenkovova2008@gmail.com>
2024-11-18 22:29:33 -0330

    CustomUser added

--- auth_sys/models.py
+++ auth_sys/models.py
@@ -11,7 +11,7 @@ class CustomUser(AbstractBaseUser):
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-
+        
     USERNAME_FIELD = "email"
 
     def __str__(self):
