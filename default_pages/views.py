b56814d51d344593fb8dc4e7ffcd4d5b587f9dff
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-11-22 14:26:10 -0330

    Changes in default_pages, templates/default_pages

--- default_pages/views.py
+++ default_pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 # Create your views here.
+class MainView(TemplateView):
+    template_name = 'default_pages/main.html'
