85269ac9b02c77ff408cffd00b326ec00a719a53
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-12-01 20:25:46 -0330

    Small changes in templates: logo, favicon. Auth_sys + logout

--- auth_sys/views.py
+++ auth_sys/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from .models import CustomUser
 from .forms import CustomUserCreationForm
 
@@ -22,3 +22,6 @@ class RegisterView(CreateView): # - RegisterView for registering new users
 class CustomLoginView(LoginView):
     template_name = 'auth_sys/login.html'
     redirect_authenticated_user = True
+
+class CustomLogoutView(LogoutView):
+    next_page = 'main'
