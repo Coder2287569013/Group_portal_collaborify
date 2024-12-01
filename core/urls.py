3a4dda212247e32c5dfc877e0fa8c97b763199ab
Vladimir <101576295+Coder2287569013@users.noreply.github.com>
2024-12-01 10:27:03 -0330

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('auth/', include('auth_sys.urls')),
     path('school/', include('grade_system.urls')),
     path('', include('default_pages.urls')),
+    path('forum', include('forum.urls')),
+    path('portfolio', include('portfolio.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
