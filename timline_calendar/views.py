86018a5ba28eb73d741a03450229d28d63bd7f44
unknown <bestrostik@gmail.com>
2024-12-18 20:05:34 +0200

    traking events done

--- timline_calendar/views.py
+++ timline_calendar/views.py
@@ -8,44 +8,20 @@ from datetime import datetime, timedelta
 def generate_timeline_with_events(start_date, end_date):
     timeline = []
     current_date = start_date
-    events = Event.objects.filter(date__range=[start_date, end_date])  # Отримуємо події в діапазоні дат
+    events = Event.objects.filter(date__range=[start_date, end_date])
 
     while current_date <= end_date:
-        # Події для поточного дня
         daily_events = events.filter(date=current_date)
         timeline.append({
-            "day": current_date.strftime("%d"),  # День місяця
-            "month": current_date.strftime("%b"),  # Місяць
-            "weekday": current_date.strftime("%a"),  # День тижня
-            "events": daily_events,  # Події
+            "day": current_date.strftime("%d"),
+            "month": current_date.strftime("%b"),
+            "weekday": current_date.strftime("%a"),
+            "events": daily_events,
         })
         current_date += timedelta(days=1)
 
     return timeline
 
-# def generate_timeline_with_events(start_date, end_date):
-#     timeline = []
-#     current_date = start_date
-#     events = Event.objects.filter(date__range=[start_date, end_date])
-
-#     while current_date <= end_date:
-#         daily_events = events.filter(date=current_date)
-#         timeline.append({
-#             "day": current_date.strftime("%d"),
-#             "month": current_date.strftime("%b"),
-#             "weekday": current_date.strftime("%a"),
-#             "events": daily_events,
-#         })
-#         current_date += timedelta(days=1)
-
-#     return timeline
-
-# def timeline_calendar(request):
-#     today = datetime.today()
-#     start_date = today - timedelta(days=7)
-#     end_date = today + timedelta(days=7)
-
-#     timeline = generate_timeline_with_events(start_date, end_date)
 def timeline_calendar(request):
     Event.objects.filter(date__lt= datetime.now() ).delete()
 
