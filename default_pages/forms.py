0768e7639f275b62ea5dfbd4f8028b353834ed22
Arrxeen <134214973+Arrxeen@users.noreply.github.com>
2024-12-21 21:22:29 +0100

    Update forms.py

--- default_pages/forms.py
+++ default_pages/forms.py
@@ -1,6 +1,7 @@
 # Here will be the forms for News, Calendar, Events and so on...
 from django import forms
-from default_pages.models import News, Event, Notification
+from default_pages.models import News, Event
+
 
 class NewsForm(forms.ModelForm):
     class Meta:
